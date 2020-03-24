6277bbdbfe52a99f36d67bdf710def88197208e3
Maximilien Drules <maximilien@localhost.localdomain>
2020-03-24 11:02:44 +0100

    first commit

---
+++ src/lemin_main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** CPE_lemin_2019
+** File description:
+** main.c
+*/
+
+#include "../include/my.h"
+
+int main(int ac, char *av[])
+{
+    (void)ac;
+    (void)av;
+    return (84);
+}
